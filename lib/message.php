201e600b30947b4f77725f94bd0a7bd22c6feabf
varun1505 <varun1505@gmail.com>
2013-11-09 15:41:21 +0530

    Initial Commit.

---
+++ lib/message.php
@@ -0,0 +1,55 @@
+<?php
+class Message {
+	/**
+	 * Id of the person who sends the message
+	 */
+	private $from;
+	/**
+	 * Serialized array of the ID's of people to whom the message is to be sent.
+	 */
+	private $to;
+	/**
+	 * Text of the message
+	 */
+	private $message;
+	/**
+	 * The date on which the Message was sent.
+	 */
+	private $sentDate;
+	
+	function getFrom(){
+		return $this->from;
+	}
+	
+	function setFrom($from){
+		$this->from = $from;
+		return $this;
+	}
+	
+	function getTo(){
+		return $this->to;
+	}
+	
+	function setTo($to){
+		$this->to = $to;
+		return $this;
+	}
+	
+	function getMessage(){
+		return $this->message;
+	}
+	
+	function setMessage($msg){
+		$this->message = $msg;
+		return $this;
+	}
+	
+	function getDate(){
+		return $this->sentDate;
+	}
+	
+	function setDate($date){
+		$this->sentDate = $date;
+		return $this;
+	}
+}
---
+++ vmp.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * 
+ * @package   vmp
+ * @author    Varun Srinivas <varun@sudosaints.com>
+ * @license   GPL-2.0+
+ * @link      http://sudosaints.com
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Varun's Message Plugin
+ * Plugin URI:        http://varun1505.com
+ * Description:       A plugin that allows users to send messages to each other.
+ * Version:           1.0.0
+ * Author:            Varun Srinivas
+ * Author URI:        http://varun1505.com
+ * Text Domain:       vmp
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/varun1505/vmp
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+require_once( plugin_dir_path( __FILE__ ) . 'vmp.class.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'vmp-admin.class.php' );
+
+/*
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ * When the plugin is deleted, the uninstall.php file is loaded.
+ *
+ * TODO:
+ *
+ * - replace Plugin_Name with the name of the class defined in
+ *   `class-plugin-name.php`
+ */
+register_activation_hook( __FILE__, array( 'vmp', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'vmp', 'deactivate' ) );
+
+/*
+ * TODO:
+ *
+ * - replace Plugin_Name with the name of the class defined in
+ *   `class-plugin-name.php`
+ * - replace Plugin_Name_Admin with the name of the class defined in
+ *   `class-plugin-name-admin.php`
+ */
+add_action( 'plugins_loaded', array( 'vmp', 'get_instance' ) );
+add_action( 'plugins_loaded', array( 'vmp', 'get_instance' ) );
